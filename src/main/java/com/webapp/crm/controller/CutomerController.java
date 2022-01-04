61d37edcbe54006017aac08a50a926e81f3bac33
adityapawar444@gmail.com <adityapawar444@gmail.com>
2022-01-04 12:54:37 +0530

    converting dynamic web project into Maven project

--- src/main/java/com/webapp/crm/controller/CutomerController.java
+++ src/main/java/com/webapp/crm/controller/CutomerController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.webapp.crm.dao.CustomerDAO;
 import com.webapp.crm.entity.Customer;
 import com.webapp.crm.service.CustomerServices;
 
