469afb900f3bb7b4b914fac79a9079b48b5a593c
adityapawar444@gmail.com <adityapawar444@gmail.com>
2022-01-20 20:07:19 +0530

    REST CRUD operations support

--- src/main/java/com/webapp/crm/config/SecurityConfig.java
+++ src/main/java/com/webapp/crm/config/SecurityConfig.java
@@ -30,6 +30,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		
+		http.csrf().disable();
 
 		http.authorizeRequests()
 		//anyRequest().authenticated(). (for login without role based access)
--- src/main/java/com/webapp/crm/rest/CustomerRestController.java
+++ src/main/java/com/webapp/crm/rest/CustomerRestController.java
@@ -3,8 +3,12 @@ package com.webapp.crm.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,30 +17,66 @@ import com.webapp.crm.rest.exceptions.CustomerNotFoundException;
 import com.webapp.crm.service.CustomerServices;
 
 @RestController
-@RequestMapping("api/customers")
+@RequestMapping("api/")
 public class CustomerRestController {
 
 	@Autowired
 	CustomerServices customerServices;
 
-	@GetMapping("list")
+	@GetMapping("customers")
 	public List<Customer> lisCustomers() {
 
 		List<Customer> resList = customerServices.getCustomersList();
 
 		return resList;
 	}
-	
-	@GetMapping("customer/{customerId}")
+
+	@GetMapping("customers/{customerId}")
 	public Customer getCustomerById(@PathVariable int customerId) {
-		
+
 		Customer customer = customerServices.getCustomerByID(customerId);
+
+		if (customer == null) {
+			throw new CustomerNotFoundException("Customer id not found : " + customerId);
+		}
+
+		return customer;
+	}
+
+	@PostMapping("customers")
+	public Customer addCutsomer(@RequestBody Customer newCustomer) {
+
+		newCustomer.setId(0);
+
+		customerServices.createNewCustomer(newCustomer);
+
+		return newCustomer;
+
+	}
+	
+	@PutMapping("customers")
+	public Customer updateCutsomer(@RequestBody Customer newCustomer) {
+
+		newCustomer.setId(0);
+
+		customerServices.createNewCustomer(newCustomer);
+
+		return newCustomer;
+
+	}
+	
+	@DeleteMapping("customers/{customerId}")
+	public String deletCUstomer(@PathVariable int customerId) {
 		
-		if(customer == null) {
+		Customer tempCustomer = customerServices.getCustomerByID(customerId);
+		
+		if(tempCustomer == null) {
 			throw new CustomerNotFoundException("Customer id not found : " + customerId);
 		}
 		
-		return customer;
+		customerServices.delete(customerId);
+		
+		return "Deleted Customer with ID : " + customerId;
 	}
 
 }
